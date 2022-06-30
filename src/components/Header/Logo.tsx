2e27d694ee250adf1cc8d59bf727603db31c4463
Cardoso <cardosoassuncao@gmail.com>
2022-06-30 11:42:29 -0400

    componentize the Header component

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+export function Logo() {
+  return (
+    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">dash<Text as="span" color="pink.500">go</Text></Text>
+  )
+}
