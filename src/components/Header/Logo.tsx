48584cd4e2b68dfe62a85ae45a66fd39ea0193c5
Cardoso <cardosoassuncao@gmail.com>
2022-07-04 19:44:18 -0400

    add responsivite on Header component

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,6 +2,13 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
   return (
-    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">dash<Text as="span" color="pink.500">go</Text></Text>
+    <Text
+      fontSize={["2xl", "3xl"]}
+      fontWeight="bold"
+      letterSpacing="tight"
+      w="64"
+    >
+      dash<Text as="span" color="pink.500">go</Text>
+    </Text>
   )
 }
--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+type ProfileProps = {
+  showProfileData?: boolean
+}
+
+export function Profile({showProfileData=true}: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Cardoso Assunção</Text>
-        <Text color="gray.300" fontSize="small">cardosoassuncao@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Cardoso Assunção</Text>
+          <Text color="gray.300" fontSize="small">cardosoassuncao@gmail.com</Text>
+        </Box>      
+      )}
       <Avatar size="md" name="Cardoso Assunção" src="https://github.com/ajcadev.png" />
     </Flex>
   )
